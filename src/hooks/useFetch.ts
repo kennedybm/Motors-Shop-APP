40109a622cf80cad3f04cfc8510dfec3e97f579f
kennedybm <kennedy_muniz@hotmail.com>
2023-05-06 15:17:22 -0300

    fix: removing axios

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,30 +1,24 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import axiosInstance from "../services/api"
+import { useEffect, useState } from "react";
+import axiosInstance from "../services/api";
 
+export default function useFetch(url: string) {
+  const [data, setData] = useState<any>();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-export default function useFetch(url: string){
+  useEffect(() => {
+    (async function refetch() {
+      try {
+        setLoading(true);
+        const response = await axiosInstance.get(url);
+        setData(response.data);
+      } catch (err: any) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
 
-    const [data, setData] = useState<any>()
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState<boolean>(false)
-
-    useEffect(() => {
-        (
-            async function refetch(){
-                try{
-                    setLoading(true)
-                    const response = await axiosInstance.get(url)
-                    setData(response.data)
-                }catch(err: any){
-                    setError(err)
-                }finally{
-                    setLoading(false)
-                }
-            }
-        )()
-    },[])
-
-    return { data, error, loading }
-
+  return { data, error, loading };
 }
